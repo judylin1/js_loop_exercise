8005757e9aab5a4838ba2625d7e41f4a301966d9
Judy Lin <judith.s.lin@gmail.com>
2015-07-14 08:48:26 -0600

    completed and MB checked

--- spec/animalSpec.js
+++ spec/animalSpec.js
@@ -1,54 +1,55 @@
 var Animal = require('../src/animal.js');
 var animal;
 
-describe('Animal', function(){
+describe('Animal', function() {
 
-  beforeEach(function(){
+  beforeEach(function() {
     // create a new animal object each time
     animal = new Animal("Fido", 27, "Pig");
   });
 
-  describe('#oink', function(){
-    it('should oink if it is a pig', function(){
-
+  describe('#oink', function() {
+    it('should oink if it is a pig', function() {
+      expect(animal.oink()).toEqual("oink");
     });
-    it('should not oink if it is not pig', function(){
-
-    });
-  });
-
-  describe('#growUp', function(){
-    it('should increment the age by one', function(){
-
+    it('should not oink if it is not pig', function() {
+      animal.kind = "Lion";
+      expect(animal.oink()).toEqual("I am not a pig");
     });
   });
 
-  describe('#awake', function(){
-    it('should initially be off', function(){
-
+  describe('#growUp', function() {
+    it('should increment the age by one', function() {
+      animal.growUp();
+      expect(animal.age).toEqual(28);
     });
   });
 
-   describe('#wakeUp', function(){
-    it('should change state to awake', function(){
-
+  describe('#awake', function() {
+    it('should initially be off', function() {
+      expect(animal.awake).toEqual(false);
     });
   });
 
-  describe('#sleep', function(){
-    it('should initially be sleeping', function(){
-
+   describe('#wakeUp', function() {
+    it('should change state to awake', function() {
+      expect(animal.wakeUp()).toEqual(true);
     });
   });
 
-  describe('#feed', function(){
-    it('should get fed if awake', function(){
-
+  describe('#sleep', function() {
+    it('should change state to asleep', function() {
+      expect(animal.sleep()).toEqual(false);
     });
-    it('not should get fed if sleeping', function(){
+  });
 
+  describe('#feed', function() {
+    it('should get fed if awake', function() {
+      animal.wakeUp();
+      expect(animal.feed()).toEqual('Feed me');
+    });
+    it('not should get fed if sleeping', function() {
+      expect(animal.feed()).toEqual('I asleep');
     });
   });
 });
-
-
--- src/animal.js
+++ src/animal.js
@@ -1,9 +1,44 @@
-function Animal(){
-
+function Animal(name, age, kind, awake) {
+  //order doesn't matter here. In animalSpec.js:
+  //animal = new Animal("Fido", 27, "Pig") so it's name, age, kind
+  this.name = name;
+  this.kind = kind;
+  this.age = age;
+  this.awake = false;
 }
 
-Animal.prototype.oink = function(){
+Animal.prototype.oink = function() {
+  if (this.kind === "Pig") {
+    return "oink";
+    }
+  else {
+    return "I am not a pig";
+  };
+};
 
+Animal.prototype.growUp = function() {
+  this.age++;
+};
+
+Animal.prototype.awake = "off";
+
+Animal.prototype.wakeUp = function() {
+  this.awake = true;
+  return this.awake;
+};
+
+Animal.prototype.sleep = function() {
+  this.awake = false;
+  return this.awake;
+};
+
+Animal.prototype.feed = function() {
+  if (this.awake === true) {
+    return 'Feed me';
+  }
+  else if (this.awake === false) {
+    return 'I asleep';
+  }
 };
 
 module.exports=Animal;
